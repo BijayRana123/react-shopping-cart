e8752068fe69240ce101fb978dce04061226fbc6
Bijay <ranabijay735@gmail.com>
2021-09-22 16:31:00 +0545

    create product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 //feature-1
+import data from './data.json';
+import { useState } from 'react'
+import Products from './components/Products';
+
 function App() {
+  const products = data.products;
+  const [size, setSize] = useState("")
+  const [sort, setSort] = useState("")
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+              <Products products={products} />
+          </div>
+          <div className="sidebar">
+                 Sidebar
+          </div>
+        </div>
       </main>
       <footer>
         All Rights is Reserved..
