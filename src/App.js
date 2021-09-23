c091380bfa1b5ca182dbbc1c24255406fc393180
Bijay <ranabijay735@gmail.com>
2021-09-23 11:27:15 +0545

    filter-component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,32 @@
 import data from './data.json';
 import { useState } from 'react'
 import Products from './components/Products';
+import Filter from './components/Filter';
 
 function App() {
-  const products = data.products;
+  const [products, setProducts] = useState([])
   const [size, setSize] = useState("")
   const [sort, setSort] = useState("")
+ 
+  const sortProducts=(event)=>{
+      setSort(event.target.value)
+      setProducts(products.slice().sort((a,b)=>(
+        sort === "lowest"?((a.price < b.price) ? 1: -1):
+        sort === "highest"?((a.price > b.price) ? 1: -1):
+        ((a._id > b._id)?1: -1)
+      )))
+  
+  }
+  const filterProducts=(event)=>{
+    if(event.target.value === ""){
+      setSize(event.target.value);
+      setProducts(data.products)
+    }else{
+      setSize(event.target.value)
+      setProducts(data.products.filter((product) => product.availableSizes.indexOf(event.target.value) >= 0))
+    }
+  }
+
   return (
     <div className="grid-container">
       <header>
@@ -15,6 +36,10 @@ function App() {
       <main>
         <div className="content">
           <div className="main">
+            <Filter count={products.length} 
+                 size={size} sort={sort}
+                 filterProducts={filterProducts}
+                 sortProducts={sortProducts} />
               <Products products={products} />
           </div>
           <div className="sidebar">
